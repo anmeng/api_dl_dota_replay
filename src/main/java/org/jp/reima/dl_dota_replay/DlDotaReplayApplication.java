bb0bd6ccfd733975aac6ce37767aeb0bddac6f22
reima4038 <reima4038@gmail.com>
2018-01-02 00:00:34 +0900

    パッケージ構成をコミット

---
+++ src/main/java/org/jp/reima/dl_dota_replay/DlDotaReplayApplication.java
@@ -0,0 +1,12 @@
+package org.jp.reima.dl_dota_replay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlDotaReplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlDotaReplayApplication.class, args);
+	}
+}
---
+++ src/test/java/org/jp/reima/dl_dota_replay/DlDotaReplayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.jp.reima.dl_dota_replay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DlDotaReplayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
